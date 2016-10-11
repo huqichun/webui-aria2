ae96081bfb7618378ae7ac84eea2cc77eafbeeb8
ihosts <huqichun@users.noreply.github.com>
2016-10-12 00:37:13 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -30,15 +30,7 @@ function mergeTranslation(translation, base) {
 webui.config(['$translateProvider', function ($translateProvider) {
   $translateProvider
       .translations('en_US', translations.en_US)
-      .translations('nl_NL', mergeTranslation(translations.nl_NL, translations.en_US))
-      .translations('th_TH', mergeTranslation(translations.th_TH, translations.en_US))
       .translations('zh_CN', mergeTranslation(translations.zh_CN, translations.en_US))
-      .translations('pl_PL', mergeTranslation(translations.pl_PL, translations.en_US))
-      .translations('fr_FR', mergeTranslation(translations.fr_FR, translations.en_US))
-      .translations('de_DE', mergeTranslation(translations.de_DE, translations.en_US))
-      .translations('es_ES', mergeTranslation(translations.es_ES, translations.en_US))
-      .translations('ru_RU', mergeTranslation(translations.ru_RU, translations.en_US))
-      .translations('it_IT', mergeTranslation(translations.it_IT, translations.en_US))
       .determinePreferredLanguage();
 }]);
 
